ef53f14ee9c01cfa75a704cc60e1de119c6525f6
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-08-23 15:22:25 +0800

    Footer added for demo purpose

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='footer bg-slate-700 p-4 text-center text-white fixed bottom-0 w-full'>
+      <p>&copy; {new Date().getFullYear()} All rights reserved</p>
+      <p>Contact us: XXXX</p>
+    </footer>
+  )
+}
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,20 +1,22 @@
 
 import { NavLink, Outlet } from 'react-router-dom'
+import Footer from './Footer'
 
 export default function Header() {
   return (
     <div className="bg-white min-h-screen">
       <header className='bg-red-700 p-6'>
         <nav className='flex justify-center items-center text-2xl'>
-          <div className='space-x-2'>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/crud">Crud Operation</NavLink>
+          <div className='space-x-2 '>
+            <NavLink to="/" className="text-white hover:text-gray-300">Home</NavLink>
+            <NavLink to="/crud" className="text-white hover:text-gray-300">Crud</NavLink>
           </div>
         </nav>
       </header>
       <main>
         <Outlet />
       </main>
+      <Footer />
     </div>
   )
 }
