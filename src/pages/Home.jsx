8aa5cb2b03d89c67177229c564cd7c75583bbccc
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-08-23 15:00:54 +0800

    Home component added for routing purposes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+// import React, { useState } from 'react';
+// import Button from '../components/Button';
 
-export default function Home() {
+export default function Home({ handleCreate }) {
+  // const [isEditing, setIsEditing] = useState(false);
+
   return (
-    <div>Home</div>
-  )
+    <div className='flex justify-center items-center min-h-screen'>
+      <div className='text-center'>
+        <p className='text-gray-900'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quis nobis aut iste, tempore animi nam fuga repudiandae praesentium quidem!
+        </p>
+        {/* {!isEditing && (
+          <div className='mt-4'>
+            <Button type="create" onClick={handleCreate}> 
+              Create New Item
+            </Button>
+          </div>
+        )} */}
+      </div>
+    </div>
+  );
 }
