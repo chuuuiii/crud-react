78e7b3cc61255f0dba092a5e742d3cd48714255c
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-08-24 17:05:09 +0800

    Home component updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default function Home({ handleCreate }) {
   return (
     <div className='flex justify-center items-center min-h-screen'>
       <div className='text-center'>
-        <p className='text-gray-900'>
+        <p className='text-gray-900 text-2xl'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quis nobis aut iste, tempore animi nam fuga repudiandae praesentium quidem!
         </p>
         {/* {!isEditing && (
